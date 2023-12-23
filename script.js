067820a4bb2d1ea033816652a6a6dbe4645918eb
Đorđe Molnar <molnarkg7@gmail.com>
2023-12-23 20:08:43 +0100

    add functionality
    Update script.js
    Update index.html (add ids)

---
+++ script.js
@@ -0,0 +1,19 @@
+const countDate = new Date("Aug 25, 2024 17:00:00").getTime(); //target - my birthday
+
+let a = setInterval(() => {
+  const today = new Date().getTime();
+
+  let difference = countDate - today;
+
+  let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
